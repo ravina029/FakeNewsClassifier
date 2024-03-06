cb88e1cf99ab8fafd3e0da28149deaad21b6d256
ravina <vermaravina029@gmail.com>
2024-03-06 12:23:41 +0200

    application created

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+stemmer= PorterStemmer()
+vectorizer=TfidfVectorizer()
+
+vector=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('pac.pkl','rb'))
+
+
+
+
+
+def fake_news(news):
+    
+    input_data=[news]
+    vector_form1=vector.transform(input_data)
+    prediction = model.predict(vector_form1)
+    return prediction
+
+
+
+if __name__ == '__main__':
+    st.title('Fake News Identifier ')
+    st.subheader("Input the News below")
+    sentence = st.text_area("Enter news", "",height=200)
+    predict_bt = st.button("predict")
+    if predict_bt:
+        prediction_class=fake_news(sentence)
+        print(prediction_class)
+        if prediction_class == [0]:
+            st.success('Reliable')
+        if prediction_class == [1]:
+            st.warning('Fake')
